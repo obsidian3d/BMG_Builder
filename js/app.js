99bf7a747888198d137b8a1db908e91fa3be82b2
Charles Silbernagel <csilbernagel@matrix-solutions.com>
2015-09-23 14:02:47 -0600

    Added filtering to character list.

--- js/app.js
+++ js/app.js
@@ -5,6 +5,8 @@ var app = angular.module('bmgApp', []);
 
 app.controller('bmgAppCtrl', function($scope, $http)
 {
+    var searchInput;
+
     //Load data for app from JSON files
     $http.get('appData/crews.json')
         .success(function (response) {$scope.crews = response.crews;});
